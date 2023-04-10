f76f8f298b4a0dce7b8f51987098a7a7308d3a08
kouki512 <ht20a008@oecu.jp>
2023-04-10 23:40:54 +0900

    add: DM機能に関するモデルの作成、アソシエーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,10 @@ class User < ApplicationRecord
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :followings, through: :relationships, source: :followed
 
+  # DM機能
+  has_many :entries, dependent: :destroy
+  has_many :messages, dependent: :destroy
+
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
