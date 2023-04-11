3e45b328868cb087775b0884f2a4ad3595540054
kouki512 <ht20a008@oecu.jp>
2023-04-11 21:52:45 +0900

    done: 7b (前日、先週比較)

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,2 +1,11 @@
 module BooksHelper
+  def compared_to_previous_day(today,yesterday)
+    unless today == 0 || yesterday == 0
+      percent = today / yesterday * 100
+      percent_str = percent.to_s + "%"
+      return percent_str
+    else
+      "--"
+    end
+  end
 end
